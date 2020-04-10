0282c451480e373aed4dede7c78308360579d77f
cecimermoz <cecii.mermoz@gmail.com>
2020-04-10 15:32:42 -0300

    Initial server

---
+++ scripts/001-createTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE competencias(
+    id INT(3) AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50)
+);
+
+INSERT INTO competencias
+VALUES (
+(1, '¿Cuál es la mejor película?')
+(2, "¿Qué drama te hizo llorar más?");
+(3, "¿Cuál es la peli más bizarra?")
+);
---
+++ servidor/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const port = 3000;
+
+server.use(cors());
+server.listen(port, function(){
+    console.log("Listening on port " + port);
+});
